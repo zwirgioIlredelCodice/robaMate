f47f3119c88a2828ad6a393a1f4f979a7920b5c5
Fabio Murer <>
2021-04-14 16:58:41 +0000

    ellu

--- combinatorio.py
+++ combinatorio.py
@@ -1,7 +1,20 @@
 import math
 
+def input_n():
+  n = 0
+  while n <= 0 or n > 500:
+    n = int(input("immetti n > 0 e < 500: "))
+  return n
+
+def input_k():
+  k = 0
+  while k <= 0 or k > 500:
+    k = int(input("immetti k > 0 e < 500: "))
+  return k
+
+
 def raggruppamenti():
-  tot = 0
+  tot = 1
   k=int(input("quanti gruppi vuoi inserire? "))
   for i in range(k):
     n=int(input("inserisci n: "))
@@ -10,41 +23,53 @@ def raggruppamenti():
 
 
 def disposizioni():
-  tot = 0
-  n=int(input("Inserisici il numero totale di elementi: "))
-  k=int(input("Inserisci quanti elementi vuoi considerare: "))
-  for i in range(0, k-1):
-    tot=tot*n-i
+  tot = 1
+  while True:
+    n=input_n()
+    k=input_k()
+    if k<n+1:
+      break
+  for i in range(k):
+    tot=tot*(n-i)
   print("il risultato dell disposizione è ",tot)
 
 
 def disposizioni_r():
-  n=int(input("Inserisici il numero totale di elementi: "))
-  k=int(input("Inserisci quanti elementi vuoi considerare:"))
+  n=input_n()
+  k=input_k()
   print("il risultato dell disposizione è ",n**k)
 
 
 def permutazioni():
   tot = 0
-  n=int(input("Inserisci il numero di elementi: "))
+  n=input_n()
   tot=math.factorial(n)
   print("il risultato dell permutazione è ",tot)
 
+def permutazione_r():
+  tot = 0
+  n=input_n()
+  ripetuti = []
+  n_ripetuti = int(input("quanti sono gli elementi ripetuti?"))
+  
+
+  print("il risultato dell permutazione è ",tot)
+
+
 def combinazione():
-  n=int(input("Inserisci il numero di elementi: "))
-  k=int(input("Inserisci il numero di elementi: "))
-  nsom=1
-  ksom=1
-  nksom=1
-  nsom=math.factorial(n)
-  ksom=math.factorial(k)
-  nksom=math.factorial(n-k)
-  tot=nsom/(ksom*nksom)
+  while True:
+    n=input_n()
+    k=input_k()
+    if k<n:
+      break
+    else:
+      print("k deve essere minore di n")
+  tot=math.factorial(n)/(math.factorial(k)*math.factorial((n-k)))
   print("il risultato dell permutazione è ",tot)
 
 def combinazioni_r():
-  n=int(input("Inserisci il numero di elementi: "))
-  k=int(input("Inserisci il numero di elementi: "))
+  n=input_n()
+  k=input_k()
   risultato = math.factorial((n+k-1))/(math.factorial(k)*math.factorial(n-1)) #https://www.youmath.it/lezioni/probabilita/calcolo-combinatorio/1217-combinazione-con-ripetizione.html
   print("il risultato dell permutazione è ",risultato)
 
--- main.py
+++ main.py
@@ -1,78 +1,49 @@
 from combinatorio import*
 mol=1
-while True:
-  dec = input("Conta l'ordine? si/no")
-  if dec== "si" or dec== "no":
-    break
-
-if dec=="si":
-  dec = input("è già suddiviso in gruppi?  si/no")
-
+while mol==1:
   while True:
-    dec=input("")
-    if dec=="si" or dec=="no":
+    dec = input("Conta l'ordine? si/no: ")
+    if dec== "si" or dec== "no":
       break
-
-  if dec=="si": #raggruppamenti
-    break
-   
-  
-  if dec == "no": #3
-    dec = input("k = n?  si/no")
+  if dec=="si":
     while True:
-      dec=input("")
+      dec = input("sono presenti ripetizioni?  si/no: ")
       if dec=="si" or dec=="no":
         break
-
-    while True:
-      dec=input("")
-      if dec=="si" or dec=="no":
-        break
-
-    if dec == "si": #4
-      dec = input("si può ripetere?  si/no")
-
+    if dec == "no": 
+      disposizioni()
+      mol=0
+    if dec=="si": 
       while True:
-        dec=input("")
+        dec = input("c'è almeno un elemento uguale ad un altro?  si/no: ")
         if dec=="si" or dec=="no":
           break
-      
-      if dec == "si":#5 disposizioni con ripetizioni
-        #disposizioni con ripetizioni
-      
-      if dec == "no": #6
-        #disposizioni
-      else:
-        print("risposta non valida")
-
-    elif dec = "no": #7
-      dec = input("si può ripetere?  si/no")
-
+      if dec=="si": 
+        permutazione_r()
+        mol=0
+      if dec=="no":
+        disposizioni_r()
+        mol=0
+  elif dec=="no":   
       while True:
-        dec=input("")
-        if dec=="si" or dec=="no"
+        dec = input("ci sono più elementi di certi insiemi che puoi ragruppare in successione ; devi associare degli elementi di un gruppo d un altro? si/no: ")
+        if dec=="si" or dec=="no":
           break
-      if dec = "si":#8
-        #permutazioni con ripetizioni
-      
-      elif dec == "no": #9
-        #permutazione
-      
-      
-    
-    
-elif dec == "no": #10
-  dec = input("si può ripetere?  si/no")
-
-  while True:
-      dec=input("")
-      if dec=="si" or dec=="no"
-        break
-  if dec = "si":#11
-      #permutazioni con ripetizioni
-     elif dec == "no": #10
-      #combinazione semplice
-  
+      if dec=="si":
+        raggruppamenti()
+        mol=0
+      if dec=="no":
+        while True:
+          dec = input("possono esserci ripetizioni? si/no: ")
+          if dec=="si" or dec=="no":
+            break
+        if dec=="si":
+          combinazioni_r()
+          mol=0
+        if dec=="no":
+          combinazione()
+          mol=0
     
 
+    
 
